f30aef3079777dae2094c7e63ec546729940a983
Pawel Kaczynski <pawel.kaczynski@digirati.co.uk>
2015-09-23 13:51:32 +0100

    Added success callback module

---
+++ FluentPolicy.Tests/Modules/Callback/SuccessCallbackTests.cs
@@ -0,0 +1,93 @@
+﻿using System.Threading.Tasks;
+using FluentPolicy.Modules.Callback;
+using FluentPolicy.Modules.Logging;
+using Should;
+using Xunit;
+
+namespace FluentPolicy.Tests.Modules.Callback
+{
+    public class SuccessCallbackTests
+    {
+        private const string SampleReturnString = "zxccvbnm";
+
+        [Fact]
+        public void CallbackModuleExecutesAction_IfSuccess()
+        {
+            var toBeTrue = false;
+
+            var returnStr = As.Func(TestFunction).WithPolicy()
+                .For().AllOtherExceptions().Return(SampleReturnString)
+                .AddModule(new SuccessCallback(() => toBeTrue = true))
+                .Execute();
+
+            // Make sure the test policy executed correctly
+            returnStr.ShouldEqual(SampleReturnString);
+            toBeTrue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public void CallbackModule_DoesntExecuteAction_IfFailure()
+        {
+            var toBeTrue = false;
+
+            var returnStr = As.Func(TestFunctionException).WithPolicy()
+                .For().AllOtherExceptions().Return(SampleReturnString)
+                .AddModule(new SuccessCallback(() => toBeTrue = true))
+                .Execute();
+
+            // Make sure the test policy executed correctly
+            returnStr.ShouldEqual(SampleReturnString);
+            toBeTrue.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task CallbackModuleExecutesAction_IfSuccess_Async()
+        {
+            var toBeTrue = false;
+
+            var returnStr = await As.Func(TestFunctionAsync).WithAsyncPolicy()
+                .For().AllOtherExceptions().Return(SampleReturnString)
+                .AddModule(new SuccessCallback(() => toBeTrue = true))
+                .ExecuteAsync();
+
+            // Make sure the test policy executed correctly
+            returnStr.ShouldEqual(SampleReturnString);
+            toBeTrue.ShouldBeTrue();
+        }
+
+        [Fact]
+        public async Task CallbackModule_DoesntExecuteAction_IfFailure_Async()
+        {
+            var toBeTrue = false;
+
+            var returnStr = await As.Func(TestFunctionExceptionAsync).WithAsyncPolicy()
+                .For().AllOtherExceptions().Return(SampleReturnString)
+                .AddModule(new SuccessCallback(() => toBeTrue = true))
+                .ExecuteAsync();
+
+            // Make sure the test policy executed correctly
+            returnStr.ShouldEqual(SampleReturnString);
+            toBeTrue.ShouldBeFalse();
+        }
+
+        private static async Task<string> TestFunctionAsync()
+        {
+            return SampleReturnString;
+        }
+
+        private static string TestFunction()
+        {
+            return SampleReturnString;
+        }
+
+        private static string TestFunctionException()
+        {
+            throw new TestException();
+        }
+
+        private static async Task<string> TestFunctionExceptionAsync()
+        {
+            throw new TestException();
+        }
+    }
+}
---
+++ FluentPolicy/Modules/Callback/SuccessCallback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FluentPolicy.Modules.Callback
+{
+    public class SuccessCallback : BaseModule
+    {
+        private readonly Action _action;
+
+        public SuccessCallback(Action action)
+        {
+            _action = action;
+        }
+
+        #region Overrides of BaseModule
+
+        public override void RegisterEvents<TReturn>(IPolicyEvents<TReturn> eventsSource)
+        {
+            eventsSource.ReturnValueObtained += (sender, @return) => _action();
+        }
+
+        #endregion
+    }
+}
